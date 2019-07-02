65ee227b77042218a1da59e3fee012668ae38e0b
Toxicl16 <50382805+Toxicl16@users.noreply.github.com>
2019-07-02 15:11:23 -0400

    Update Calibration.ino

--- Calibration.ino
+++ Calibration.ino
@@ -3,12 +3,12 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h> // LiquidCrystal_I2C library
 HX711_ADC LoadCell(4, 5); // parameters: dt pin, sck pin<span data-mce-type="bookmark" style="display: inline-block; width: 0px; overflow: hidden; line-height: 0;" class="mce_SELRES_start"></span>
-LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE); // 0x27 is the i2c address of the LCM1602 IIC v1 module (might differ)
+LiquidCrystal_I2C lcd(0x27,16,2); // 0x27 is the i2c address of the LCM1602 IIC v1 module (might differ)
 void setup() {
   LoadCell.begin(); // start connection to HX711
   LoadCell.start(2000); // load cells gets 2000ms of time to stabilize
-  LoadCell.setCalFactor(999.0); // calibration factor for load cell => strongly dependent on your individual setup
-  lcd.begin(16, 2); // begins connection to the LCD module
+  LoadCell.setCalFactor(400); // calibration factor for load cell => strongly dependent on your individual setup
+  lcd.begin(); // begins connection to the LCD module
   lcd.backlight(); // turns on the backlight
 }
 void loop() {
